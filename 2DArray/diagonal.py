f46ceea76bffefb332379fcc20a4cb77d09e1cae
Prabhat <parbhatkushwaha5@gmail.com>
2025-08-06 12:54:34 +0530

    Auto commit on 06-08-2025 at 12:54:34.59

---
+++ 2DArray/diagonal.py
@@ -0,0 +1,10 @@
+nums = [[9,8,7],[6,5,4],[3,2,1]]
+rows = len(nums)
+cols = len(nums[0])
+for i in range(0,rows):
+    for j in range(0,cols):
+        if j==i:
+            print(nums[i][j],end=" ")
+        else:
+            print(" ",end=" ")
+    print()
