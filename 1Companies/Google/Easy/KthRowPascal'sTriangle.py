193e4c8d1ba837bd8d98981d600da370e69bbbff
Prabhat <parbhatkushwaha5@gmail.com>
2025-10-15 14:27:31 +0530

    Auto commit on 15-10-2025 at 14:27:31.16

---
+++ 1Companies/Google/Easy/KthRowPascal'sTriangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getRow(self, A):
+        tri = []
+        for i in range(A+1):
+            row = [1]
+            if i > 0:
+                last_row = tri[i-1]
+                for j in range(1, i):
+                    row.append(last_row[j-1] + last_row[j])
+                row.append(1)
+            tri.append(row)
+        return tri[A]
+
+A = int(input("Enter number of Row: "))
+print(Solution().getRow(A))
--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-class Node:
-    def __init__(self, data):
-            self.data = data
-            self.next = None
-
-class Solution:    
-    def pairWiseSwap(self, head):
-        # code here
-        prev = head
-        curr = prev.next
-        while prev and prev.next:
-            prev = curr
-            curr = prev
-            prev = prev.next.next
-            curr = curr.next.next
-        return head
+print("Hello World")
+A = int(input("Enter number of Row: "))
+print("You entered:", A)
